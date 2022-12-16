f5bbc13665d9a113d77a2e0c5401d4070f39921b
Ivan <ivan@mail.ru>
2022-12-16 18:32:16 +0300

    home posts detail with js

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
 //NavBar
+const burger = document.getElementById("iconBar"),
+      close_burger = document.getElementById("close-icon"),
+      show_comment = document.querySelectorAll("#show-comment"),
+      reply = document.querySelectorAll("#show-reply");
+
+function showMoreBar() {
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:none;");
+    navigation.classList.remove("hide");
+}
+
+function hideMoreBar() {
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:block;");
+    navigation.classList.add("hide");
+}
+
+burger.addEventListener("click", showMoreBar);
+close_burger.addEventListener("click", hideMoreBar);
 
 //Comment
+function showComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.classList.remove("hide");
+}
+
+show_comment.forEach(btn => btn.addEventListener('click', showComment));
 
 //Reply
+function showReply(){
+    var replyArea = document.getElementById("reply-area");
+    replyArea.classList.remove("hide");
+}
+
+reply.forEach(btn => btn.addEventListener('click', showReply));
