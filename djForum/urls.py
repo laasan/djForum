d10748ea5e52ef4859394291880466dc7f276ff6
alek <email@address.com>
2022-12-18 20:15:21 +0300

    author, category, post in models + admin

--- djForum/urls.py
+++ djForum/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("main.urls"))
+    path('', include("main.urls")),
+    path('tinymce/', include('tinymce.urls')),
+    path('hitcount/', include('hitcount.urls', namespace='hitcount')),
     ]
 
