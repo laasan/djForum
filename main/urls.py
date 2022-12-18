143741e1589ee46698303af85ecf34d9a57f721d
alek <email@address.com>
2022-12-18 14:17:42 +0300

    detail and posts

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import (
-    home)
+from .views import home, detail, posts
 
 urlpatterns = [
-    path("", home, name="home")
+    path("", home, name="home"),
+    path("detail/", detail, name="detail"),
+    path("posts/", posts, name="posts"),
     ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "forums.html", {})
+
+
+def detail(request):
+    return render(request, "detail.html", {})
+
+
+def posts(request):
+    return render(request, "posts.html", {})
